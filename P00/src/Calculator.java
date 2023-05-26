3cb357a8fdf4540b00b6888230f024d7b0ce0ab3
22015949 <22015949@Yezhuo>
2023-05-26 12:56:58 +0800

    added subract method yezhuo

---
+++ P00/src/Calculator.java
@@ -0,0 +1,25 @@
+
+/**
+ * I declare that this code was written by me.
+ * I will not copy or allow others to copy my code.
+ * I understand that copying code is considered as plagiarism.
+ *
+ * 22015949, 26 May 2023 12:26:46 pm
+ */
+
+public class Calculator {
+	public int add(int a, int b) {
+		return a + b;
+	}
+	
+	public int subtract(int a,int b) {
+		return a - b;
+	}
+	public int multiple (int a, int b) {
+		return a*b;
+		
+	}
+	public int divide (int a, int b) {
+		return a/b;
+	}
+}
